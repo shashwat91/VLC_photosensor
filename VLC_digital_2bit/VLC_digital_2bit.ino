e791d35dbdbd350626c692481fb0ca2ad2d6f851
shashwat91 <91shashwat@gmail.com>
2017-03-17 13:53:53 +0100

    Added log for Test6, 3bit data

--- VLC_digital_2bit/VLC_digital_2bit.ino
+++ VLC_digital_2bit/VLC_digital_2bit.ino
@@ -5,18 +5,26 @@
 
 const int ledpin1 = 11;
 int counter1 = 0;
-bool ledState = false;
+bool ledState = true;
+int counter=0;
 
 void setup(void)
 {
   pinMode(ledpin1, OUTPUT);
-  Timer1.initialize(5000);
+  Timer1.initialize(4000);
   Timer1.attachInterrupt(blinkLED);
 }
 void blinkLED(void)
 {
+  
+  if(counter == 0)
+    ledState = !ledState;
+  else if(counter == 2)
+    ledState = !ledState;
   digitalWrite(ledpin1, ledState);
-  ledState = !ledState;
+  counter++;
+  if(counter == 3)
+    counter=0;
 }
 void loop(void)
 {}
