2985c63150d0d0084c1c97b1c7240ca354c0fa7c
shashwat91 <91shashwat@gmail.com>
2017-03-30 12:49:53 +0200

    Added formula for timer value

--- VLC_digital_2bit/VLC_digital_2bit.ino
+++ VLC_digital_2bit/VLC_digital_2bit.ino
@@ -3,27 +3,23 @@
 #define BUFFER_SIZE 5
 #define MAX 50
 
-const int ledpin1 = 13;
+const int ledpin1 = 11;
 int counter1 = 0;
-bool ledState = false;
+bool ledState = true;
 int counter=0;
 
 void setup(void)
 {
   pinMode(ledpin1, OUTPUT);
+  //* Value for timer = 10^6/freq(hz)
+  //Timer1.initialize(1000000);  //1sec interrupt
   Timer1.initialize(1000000);
   Timer1.attachInterrupt(blinkLED);
 }
 void blinkLED(void)
 {
-  /*if(counter == 0)
-    ledState = !ledState;
-  else if(counter == 2)*/
   ledState = !ledState;
   digitalWrite(ledpin1, ledState);
-  /*counter++;
-  if(counter == 3)
-    counter=0;*/
 }
 void loop(void)
 {}
